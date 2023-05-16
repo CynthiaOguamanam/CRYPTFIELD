e6d51c6885f73132a1231b8194db277ef6bbabfd
CynthiaOguamanam <cynthiadymphna04@gmail.com>
2023-05-16 21:58:37 +0100

    footer

--- src/Components/Footer/FooterStyle.js
+++ src/Components/Footer/FooterStyle.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 width: 100%;
-height: 90px;
+height: 50px;
 /* background-color: #16233C; */
 background-color: #16233C;
 display: flex;
